9a4d8b8c9f8a803c18c50a82295b3b2d75739be5
Manik Dahiya <81329364+manik28mar@users.noreply.github.com>
2023-02-26 20:13:48 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+// Initializing the carousels
+
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    autoPlay: 5000,
+    freeScroll: true,
+    wrapAround: true,
+  });
