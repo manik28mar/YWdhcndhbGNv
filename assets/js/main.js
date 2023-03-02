f1ed87998b90d8fe89d454d4c5ecc82d917699a0
Manik Dahiya <81329364+manik28mar@users.noreply.github.com>
2023-03-02 10:28:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,50 @@ $('.main-carousel').flickity({
     freeScroll: true,
     wrapAround: true,
   });
+
+  $('.mini-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    autoPlay: 900,
+    freeScroll: true,
+    wrapAround: true,
+    fade: true
+  });
+
+// Mobile nav bar opening and closing function
+  function openNav() {
+    document.getElementById("nav-bar-content").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("nav-bar-content").style.width = "0%";
+  }
+
+
+
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById('mobile-nav-bar-box').style.top = "0";
+      } else if (prevScrollpos < currentScrollPos){
+        document.getElementById('mobile-nav-bar-box').style.top = "-65px";
+      }
+
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById('nav-bar-icon').style.top = "10px";
+      } else if (prevScrollpos < currentScrollPos){
+        document.getElementById('nav-bar-icon').style.top = "-65px";
+      }
+
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById('mobile-nav-logo').style.top = "10px";
+      } else if (prevScrollpos < currentScrollPos){
+        document.getElementById('mobile-nav-logo').style.top = "-65px";
+      }
+
+      prevScrollpos = currentScrollPos;
+    }
